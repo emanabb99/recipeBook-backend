803af90a8467c9db70c94fb5ec9548d76e175652
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-12 15:53:06 +0100

    Added edit functionality to service

--- src/main/java/recipeBook/service/RecipeService.java
+++ src/main/java/recipeBook/service/RecipeService.java
@@ -5,6 +5,7 @@ import recipeBook.entity.Recipe;
 import recipeBook.repository.RecipeRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class RecipeService {
@@ -24,11 +25,21 @@ public class RecipeService {
         return recipeRepository.findAll();
     }
 
-    public void deleteRecipe(Long id) {
-        if (recipeRepository.existsById(id)) {
-            recipeRepository.deleteById(id);
-        } else {
-            throw new RuntimeException("Recipe not found");
-        }
+    public Recipe editRecipe(Recipe recipe) {
+        Optional<Recipe> recipeToEdit = recipeRepository.findById(recipe.getId());
+        Recipe existingRecipe = recipeToEdit.orElseThrow();
+        existingRecipe.setName(recipe.getName());
+        existingRecipe.setIngredients(recipe.getIngredients());
+        existingRecipe.setInstructions(recipe.getInstructions());
+        return recipeRepository.save(existingRecipe);
     }
+
+
+public void deleteRecipe(Long id) {
+    if (recipeRepository.existsById(id)) {
+        recipeRepository.deleteById(id);
+    } else {
+        throw new RuntimeException("Recipe not found");
+    }
+}
 }
