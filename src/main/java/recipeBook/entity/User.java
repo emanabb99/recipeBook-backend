797c0621c6111c511049e03a197c0cb7e8fac8c6
Eman Abbas <eman.abbas@autotrader.co.uk>
2026-06-13 18:19:44 +0100

    Made email unique for users so you cant have multiple

--- src/main/java/recipeBook/entity/User.java
+++ src/main/java/recipeBook/entity/User.java
@@ -16,6 +16,7 @@ public class User {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
 
